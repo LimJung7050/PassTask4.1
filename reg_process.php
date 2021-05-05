bc58098affcf8ab1c959ee72f00fe2831d4c8710
Raymond Ngo Wei Kiat <ngoraymond72@gmail.com>
2021-05-05 22:13:15 +0800

    add check registration

--- reg_process.php
+++ reg_process.php
@@ -2,9 +2,9 @@
 <html lang="en-us">
 
 <head>
-  
+
     <title>MWE | Register</title>
-    
+
 </head>
 
 <body>
@@ -15,15 +15,15 @@
 
     if (file_exists($_POST['input_nric'] . '.txt')) {     //Check for filename with given nric
         echo '<h3>You are already registered!</h3>';
-    } 
-    else {   //filename not found, write to file 
+    }
+    else {   //filename not found, write to file
         $content_to_write = "Name: " . $_POST['input_name'] . "\n";
         $content_to_write .= "NRIC: " . $_POST['input_nric'] . "\n";
         file_put_contents($_POST['input_nric'] . '.txt', $content_to_write);
         echo '<h3>Registration is successful!</h3>';
-    } 
+    }
 
     ?>
-   
+
 </body>
 </html>
